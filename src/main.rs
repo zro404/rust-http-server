b2bdc501fca48d5a99515b22445d690d59514b8f
Harish G <dev.harish3124@gmail.com>
2024-12-25 13:11:58 +0530

    Create single threaded server

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6969").expect("Could not bind to port");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_request(stream);
+    }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|line| line.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "public/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "public/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
